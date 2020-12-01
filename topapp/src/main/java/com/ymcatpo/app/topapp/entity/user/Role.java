5d34cbeb96f92514a51144424ff446b545e1b5dc
shubham garg <shubhamgargfzr@gmail.com>
2020-12-01 18:11:34 +0530

    Added Model Classes for Authentication

---
+++ topapp/src/main/java/com/ymcatpo/app/topapp/entity/user/Role.java
@@ -0,0 +1,26 @@
+package com.ymcatpo.app.topapp.entity.user;
+
+public class Role {
+
+	int id ;
+	
+	String roleName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+	
+}
